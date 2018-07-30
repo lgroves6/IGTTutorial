264d0f9050627fdfaca297e6de5e863104fc948b
lgroves6 <32175364+lgroves6@users.noreply.github.com>
2018-07-30 11:52:04 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,34 @@
+ 
+<!DOCTYPE html> 
+<html>
+  <head>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <header class="site-header"> 
+    <div class="container">
+      <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+      <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+      <img style = "width: 6.5%; height:6.5%; padding-left:5px; " src= <?php echo get_theme_file_uri('/images/vasstlogo.png') ?> />
+      <img style = "width: 6.5%; height:6.5%; padding-left:5px; " src= <?php echo get_theme_file_uri('/images/robartslogo.png') ?> />
+      <img style = "width: 6%; height:6%; padding-left:5px;" src= <?php echo get_theme_file_uri('/images/slicerlogo.png') ?> />
+      <div class="site-header__menu group" style="float:right">
+        <nav class="main-navigation">
+          <ul class = "min-list group">
+            <li><a  href="<?php echo site_url()?>">Home page</a></li>
+            <li><a href="<?php echo site_url('/slicer-roadmap') ?>">3D Slicer architecture</a></li>
+            <li><a href="<?php echo site_url('/igt-module')?>">IGT module development</a></li>
+            <li><a href="<?php echo site_url('/igt-demo')?>">Examples and demonstrations</a></li>
+            <li><a href="<?php echo site_url('/create-module')?>">Create your module</a></li>
+          </ul>
+        </nav>
+        <div class="site-header__util">
+          <!--
+          <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+          <a href="#" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>-->
+          <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+        </div>
+      </div>
+    </div>
+  </header>
+    	
